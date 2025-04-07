65e5c888230f776f768cdf9682884a76bcc9f0b8
LirOoo <592645943@qq.com>
2025-04-07 18:53:25 +0800

    Update application

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# main.py
 from flask import Flask, request
 from telegram import Update
 from telegram.ext import ContextTypes
@@ -8,11 +9,8 @@ from jarvis_bot import JatvisBot  # 假设你的类写在 jatvis.py 里
 
 app = Flask(__name__)
 
-# 通过环境变量获取 TELEGRAM_ACCESS_TOKEN
-telegram_access_token = os.environ.get("TELEGRAM_ACCESS_TOKEN")
-
 # 初始化 Bot 实例（只初始化一次）
-bot = JatvisBot("config.ini", telegram_access_token)  # 传递 Telegram 访问令牌
+bot = JatvisBot("config.ini")
 bot.start("https://jatvisbot-36623340325.asia-east1.run.app/webhook")  # ✅ 注册 Webhook
 dispatcher = bot.updater.dispatcher
 
@@ -30,4 +28,6 @@ if __name__ == "__main__":
     # Cloud Run 会设置 PORT 环境变量
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+    # 读取刚才部署用的 main.py 内容，确认 Flask 是否正确监听了 PORT=8080
+   
 
